c09630460b1bb4dff74b6a0aca1ed3db556e1cf6
content-deleted <contentdeleted@gmail.com>
2018-07-28 15:32:20 -0700

    feat: added a simple jump, it's very bad and needs lots of cleaning and refactors

--- entity.c
+++ entity.c
@@ -11,22 +11,24 @@ typedef struct point {
 typedef struct animationState {
     UINT8 length; //amount of frames in animation
     UINT8 speed; //amount of ticks before moving to next frame
-    UBYTE properties; //0x1 = loop? 
+    UBYTE properties; //0x01 = loop? 0x2 = reached end of animation
     UBYTE address; //first frame in animation
     //UBYTE (*conditions[])();
 } animationState;
 
 enum direction {up=0x1, down=0x2, right=0x4, left=0x8};
 enum directionPrevious {upPrevious=0x10, downPrevious=0x20, rightPrevious=0x40, leftPrevious=0x80};
-
+enum entityFlags {facing=0x1, airborn=0x2};
 // This struct should be based off of everything a player needs but can probably be reused
 typedef struct entity {
     UBYTE direction; // Byte representing 8 boolean values. 0-3 are current , 4-7 are previous
     point position;
-    UBYTE facing; // 0 if facing left else facing right. probably update for other flags later
+    // Bit 1 is 0 if facing left else facing right.
+    // Bit 2 is 0 if on ground 1 if in air
+    UBYTE flags; 
 
     UBYTE spriteAddress; // Stores the starting position of the current spite
-    animationState * currentAnimation;
+    animationState *currentAnimation;
     // There's probably an easy way to reduce this size
     UINT8 currentFrame; // Stores the number frame (0 indexed)
     UINT8 ticks; // Stores the ticks past since switching to this frame of the animation
@@ -42,10 +44,17 @@ animationState PlayerStand = {
 };
 
 animationState PlayerRun = {
-    1U, //length zero indexed
+    2U, //length zero indexed
     10U, //speed
     0x00,
-    0x0C //first frame
+    0xC //first frame
+};
+
+animationState PlayerJump = {
+    1U, //length zero indexed
+    16U, //speed
+    0x01, //play once
+    0x14 //first frame
 };
 
 //global define functions
@@ -53,20 +62,41 @@ void updatePosition(entity *e);
 void updateDirection(entity *e);
 void setAnimation(entity *e, animationState *a);
 void updateAnimation(entity *e);
+INT8 checkTileCollision(UINT8 current, INT8 move);
 
 void updatePosition(entity *e) {
     //check flags and update animations
-    if(e->direction & 0x0F) setAnimation(e, &PlayerRun);
-    else                    setAnimation(e, &PlayerStand);
+    if(!(e->flags & airborn)) setAnimation(e, (e->direction & left) || (e->direction & right) ? &PlayerRun : &PlayerStand);
 
     //update animation 
     updateAnimation(e);
-
+ 
     //check directions and update position
-    if(e->direction & left) {e->position.x--; e->facing = 0U;}
-    if(e->direction & right) {e->position.x++; e->facing = 1U;}
+    if(e->direction & left) {
+        e->position.x--; 
+        e->flags &= ~facing;
+    }
+    if(e->direction & right) {
+        e->position.x++; 
+        e->flags |= facing;
+    }
+    
+    //check jump 
+    //this is temp code and will need to be refactored when collisions are added
+    if((joypad() & J_A) && !(e->flags & airborn)) {
+        e->flags |= airborn;
+        setAnimation(e, &PlayerJump);
+    }
 
-    if(e->facing) {
+    if(e->flags & airborn){
+        INT8 inc = checkTileCollision(e->position.y, (e->currentFrame < e->currentAnimation->length) ? -4 : 2);
+        if(!inc) e->flags &= ~airborn;
+        e->position.y += inc;
+    }
+    //
+
+
+    if(e->flags & 1) {
         set_sprite_prop(e->spriteAddress, S_FLIPX);
         set_sprite_prop(e->spriteAddress + 1U, S_FLIPX);
         move_sprite(e->spriteAddress, e->position.x + 8U, e->position.y);
@@ -85,17 +115,22 @@ void updateDirection(entity *e) {
 }
 
 void setAnimation(entity *e, animationState *a){
-    if(e->currentAnimation != a) e->currentAnimation = a;
+    if(e->currentAnimation != a) {
+        e->currentAnimation = a;
+        e->currentFrame = 0U;
+        e->ticks = 0U;
+    }
 }
 
-
-
 void updateAnimation(entity *e) {
-    if(e->ticks > e->currentAnimation->speed) {
+    if(e->ticks > e->currentAnimation->speed && e->currentFrame < e->currentAnimation->length ) {
         e->ticks = 0U; 
         e->currentFrame++;
     }
-    if(e->currentFrame > e->currentAnimation->length) e->currentFrame = 0U;
+
+    if(e->currentFrame >= e->currentAnimation->length && !(e->currentAnimation->properties & 0x01)){
+        e->currentFrame = 0U;
+    }
 
     // Here the sprite address on entity represents which sprite slot we use in sprite memory
     // The current animation address is where in VRAM the first tile of the first frame is stored
@@ -105,5 +140,16 @@ void updateAnimation(entity *e) {
     set_sprite_tile(e->spriteAddress + 0U, e->currentAnimation->address + e->currentFrame * 4U);
     set_sprite_tile(e->spriteAddress + 1U, e->currentAnimation->address + 2U + e->currentFrame * 4U);
 
-    e->ticks++;
+    //prevent overflow? (may not be important)
+    if(e->ticks < 0xFF) e->ticks++;
+}
+
+//needs to be updated when tile collisions are a thing
+// This function should return the amount to move the player
+// If there is a collision then it's the amount to that collision
+// Else it's just the movement
+INT8 checkTileCollision(UINT8 current, INT8 move) {
+    return (current+move > 0x90 || current+move <= 0x00)
+    ? 0 // When we move to tile collision itll look more like: (move % 8) * move
+    : move;
 }
